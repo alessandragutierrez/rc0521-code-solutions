7778410dfb3d5b7c3d7776a5a673b978d79df9e6
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-06-24 00:17:46 +0000

    Completed 'user can add a note'.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,9 +1,23 @@
-const dataJSON = require('./data.json');
+const fs = require('fs');
+const data = require('./data.json');
 
 const readNotes = () => {
-  for (const key in dataJSON.notes) {
-    console.log(key + ': ' + dataJSON.notes[key]);
+  for (const key in data.notes) {
+    console.log(key + ': ' + data.notes[key]);
   }
+};
+
+const addNote = () => {
+  const note = process.argv[3];
+  const noteId = data.nextId;
+  data.notes[noteId] = note;
+  data.nextId++;
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
 };
 
 const command = process.argv[2];
@@ -11,5 +25,8 @@ const command = process.argv[2];
 switch (command) {
   case 'read':
     readNotes();
+    break;
+  case 'create':
+    addNote();
     break;
 }
