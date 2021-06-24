ff2598d94a2131c863ca6c38c8ea840b077b0e79
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-06-24 00:28:41 +0000

    Completed 'user can delete a note'.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 const data = require('./data.json');
 
+const writeFile = () => {
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+};
+
 const readNotes = () => {
   for (const key in data.notes) {
     console.log(key + ': ' + data.notes[key]);
@@ -12,12 +21,17 @@ const addNote = () => {
   const noteId = data.nextId;
   data.notes[noteId] = note;
   data.nextId++;
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
+  writeFile();
+};
+
+const deleteNote = () => {
+  const targetNote = process.argv[3];
+  for (const key in data.notes) {
+    if (key === targetNote) {
+      delete data.notes[key];
     }
-  });
+  }
+  writeFile();
 };
 
 const command = process.argv[2];
@@ -28,5 +42,8 @@ switch (command) {
     break;
   case 'create':
     addNote();
+    break;
+  case 'delete':
+    deleteNote();
     break;
 }
