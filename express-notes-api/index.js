42dc6cab05f234ee8e717eda6508192fe330b251
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-06-29 21:26:43 +0000

    Completed 'GET' single note by id.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -12,6 +12,16 @@ app.get('/api/notes', (req, res) => {
   res.json(notesArray);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  if (!Number.isInteger(id) || Math.sign(id) !== 1) {
+    return res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (!data.notes[id]) {
+    return res.status(404).json({ error: 'cannot find note with id ' + id });
+  }
+  res.json(data.notes[id]);
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Express server listening on port 3000');
