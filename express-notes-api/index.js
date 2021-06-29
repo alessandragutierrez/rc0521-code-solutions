b753d15997af3bef7e3a6873c2691aa494b00295
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-06-29 22:18:43 +0000

    Completed 'PUT'(replace) note by id.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -51,6 +51,22 @@ app.delete('/api/notes/:id', (req, res) => {
   res.sendStatus(204);
 });
 
+app.put('/api/notes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  if (!Number.isInteger(id) || Math.sign(id) !== 1) {
+    return res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (!req.body.content) {
+    return res.status(400).json({ error: 'content is a required field' });
+  } else if (!data.notes[id]) {
+    return res.status(404).json({ error: 'cannot find note with id ' + id });
+  }
+  data.notes[id].content = req.body.content;
+  if (data.notes[id].content !== req.body.content) {
+    return res.status(500).json({ error: 'An unexpected error occurred.' });
+  }
+  res.status(200).json(data.notes[id]);
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Express server listening on port 3000');
