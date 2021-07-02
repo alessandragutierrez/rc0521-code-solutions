e9b70a47670669a98ff561d6f93da0d1a6b29c84
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-07-02 19:52:46 +0000

    Completed 'GET /api/grades'.

---
+++ sgt-back-end/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const pg = require('pg');
+
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+const middleware = express.json();
+const app = express();
+app.use(middleware);
+
+app.get('/api/grades', (req, res) => {
+  const sql = `
+    select "gradeId",
+           "name",
+           "course",
+           "score",
+           "createdAt"
+      from "grades"
+  `;
+  db.query(sql)
+    .then(result => {
+      const grade = result.rows;
+      res.status(200).json(grade);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
