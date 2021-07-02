16fb1b8e0b6cfcb12953059579c3915a046bc4e3
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-07-02 22:39:38 +0000

    Completed 'POST /api/grades'.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -21,10 +21,71 @@ app.get('/api/grades', (req, res) => {
            "createdAt"
       from "grades"
   `;
+
   db.query(sql)
     .then(result => {
       const grade = result.rows;
       res.status(200).json(grade);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+app.post('/api/grades', (req, res) => {
+  const invalidInputErrors = [];
+  const missingFieldMessage = ' is a required field';
+  let invalidInput = false;
+  if (!req.body.name) {
+    invalidInputErrors.push({
+      error: 'name' + missingFieldMessage
+    });
+    invalidInput = true;
+  }
+  if (!req.body.course) {
+    invalidInputErrors.push({
+      error: 'course' + missingFieldMessage
+    });
+    invalidInput = true;
+  }
+  if (!req.body.score) {
+    invalidInputErrors.push({
+      error: 'score' + missingFieldMessage
+    });
+    invalidInput = true;
+  }
+  if (req.body.score < 0 || req.body.score > 100) {
+    invalidInputErrors.push({
+      error: 'score must be a valid integer between 0 and 100'
+    });
+    invalidInput = true;
+  }
+  if (invalidInput === true) {
+    if (invalidInputErrors.length === 1) {
+      return res.status(400).json(invalidInputErrors[0]);
+    } else {
+      return res.status(400).json(invalidInputErrors);
+    }
+  }
+
+  const sql = `
+    insert into "grades" ("name", "course", "score")
+      values ($1, $2, $3)
+      returning *
+  `;
+  const gradeInput = [
+    req.body.name,
+    req.body.course,
+    req.body.score
+  ];
+
+  db.query(sql, gradeInput)
+    .then(result => {
+      const grade = result.rows[0];
+      res.status(201).json(grade);
     })
     .catch(err => {
       console.error(err);
