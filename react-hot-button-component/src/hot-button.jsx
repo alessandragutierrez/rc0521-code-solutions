56715b09a217cf9151e43c073776edf31b69bcf1
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-07-09 20:00:51 +0000

    rrevision

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -13,28 +13,31 @@ class HotButton extends React.Component {
     this.setState({ numberOfClicks: this.state.numberOfClicks + 1 });
   }
 
-  render() {
+  selectClass() {
     const numberOfClicks = this.state.numberOfClicks;
-    let className;
-    if (numberOfClicks >= 18) {
-      className = 'button button-18-clicks';
-    } else if (numberOfClicks >= 15) {
-      className = 'button button-15-clicks';
-    } else if (numberOfClicks >= 12) {
-      className = 'button button-12-clicks';
-    } else if (numberOfClicks >= 9) {
-      className = 'button button-9-clicks';
-    } else if (numberOfClicks >= 6) {
-      className = 'button button-6-clicks';
-    } else if (numberOfClicks >= 3) {
-      className = 'button button-3-clicks';
-    } else if (numberOfClicks >= 0) {
-      className = 'button button-0-clicks';
-    }
+    return (
+      numberOfClicks >= 18
+        ? 'button button-18-clicks'
+        : numberOfClicks >= 15
+          ? 'button button-15-clicks'
+          : numberOfClicks >= 12
+            ? 'button button-12-clicks'
+            : numberOfClicks >= 9
+              ? 'button button-9-clicks'
+              : numberOfClicks >= 6
+                ? 'button button-6-clicks'
+                : numberOfClicks >= 3
+                  ? 'button button-3-clicks'
+                  : 'button button-0-clicks'
+    );
+  }
+
+  render() {
+    const className = this.selectClass();
     return (
       <div className="container">
         <button onClick={this.handleClick} className={className}>Hot Button</button>
-        <h1 className="click-counter">{`${numberOfClicks} clicks`}</h1>
+        <h1 className="click-counter">{`${this.state.numberOfClicks} clicks`}</h1>
       </div>
     );
   }
