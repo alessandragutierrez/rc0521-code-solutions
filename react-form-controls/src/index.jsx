fed914df3fb740908fd9d34c03135b79d5a01480
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-07-12 22:22:29 +0000

    Completed react-form-controls.

--- react-form-controls/src/index.jsx
+++ react-form-controls/src/index.jsx
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('state:', this.state.value);
+  }
+
   render() {
     return (
-      <form>
-        <label>
-          Email
-          <input></input>
+      <form onSubmit={this.handleSubmit}>
+        <label>Email
+          <input type="email" name="email" value={this.state.value} onChange={this.handleChange} />
         </label>
         <button>Sign Up</button>
       </form>
