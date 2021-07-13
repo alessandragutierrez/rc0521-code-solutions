37f43b30890195a79824145e58262513436f67fa
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-07-13 22:56:07 +0000

    in progress

---
+++ react-accordion-component/src/accordian.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topicOpen: null
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({ topicOpen: event.target.textContent });
+  }
+
+  renderTopics() {
+    const topics = this.props.topics;
+    return (
+      topics.map(topic =>
+        <div key={topic.id}>
+          <div onClick={this.handleClick} className="topic-name">{topic.name}</div>
+          <div className={
+            this.state.topicOpen === topic.name
+              ? 'topic-desc topic-desc-open'
+              : 'topic-desc topic-desc-closed'
+              }>{topic.desc}</div>
+        </div>
+      )
+    );
+  }
+
+  render() {
+    // console.log(this.state.topicOpen);
+    const topicElements = this.renderTopics();
+    return (
+      <div className="accordion-container">{topicElements}</div>
+    );
+  }
+}
+
+export default Accordion;
