ecbf85abbf400b67c79ea006cca55e5282fa899c
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-07-15 05:30:27 +0000

    in progress.

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -3,23 +3,89 @@ import React from 'react';
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    // this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      currentImageId: this.props.images[0].id
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    return (
+      event.target.className.includes('arrow-left')
+        ? this.handleLeftArrowClick()
+        : event.target.className.includes('arrow-right')
+          ? this.handleRightArrowClick()
+          : event.target.className.includes('progress-dot')
+            ? this.handleProgressDotClick()
+            : null
+    );
+  }
+
+  handleLeftArrowClick() {
+    let currentIndex = this.props.images.findIndex(element => element.id === this.state.currentImageId);
+    if (currentIndex === 0) {
+      currentIndex = this.props.images.length;
+    }
+    this.setState({ currentImageId: this.props.images[currentIndex - 1].id });
+  }
+
+  handleRightArrowClick() {
+    let currentIndex = this.props.images.findIndex(element => element.id === this.state.currentImageId);
+    if (currentIndex === this.props.images.length - 1) {
+      currentIndex = -1;
+    }
+    this.setState({ currentImageId: this.props.images[currentIndex + 1].id });
+  }
+
+  handleProgressDotClick() {
+    const targetIndex = this.props.images.findIndex(element => element.id === event.target.id);
+    this.setState({ currentImageId: this.props.images[targetIndex].id });
   }
 
   renderImages() {
     const images = this.props.images;
     return (
       images.map(image =>
-        <img key={image.id} src={image.url}/>
+        <img key={image.id} src={image.url} id={image.id} className={
+          this.state.currentImageId === image.id
+            ? 'image-show'
+            : 'image-hide'
+        }/>
+      )
+    );
+  }
+
+  renderProgressDots() {
+    const images = this.props.images;
+    return (
+      images.map(image =>
+        <i key={image.id} onClick={this.handleClick} id={image.id} className={
+          this.state.currentImageId === image.id
+            ? 'fas fa-circle progress-dot'
+            : 'far fa-circle progress-dot'
+        }></i>
       )
     );
   }
 
   render() {
     const images = this.renderImages();
+    const progressDots = this.renderProgressDots();
     return (
-      <div>{images}</div>
+      <div className="container">
+        <div className="carousel-container row">
+          <div onClick={this.handleClick} className="fas fa-chevron-left arrow column arrow-left"></div>
+          <div className="column">
+            <div className="image-container">
+              {images}
+            </div>
+            <div className="progress-dot-container">
+              {progressDots}
+            </div>
+          </div>
+          <div onClick={this.handleClick} className="fas fa-chevron-right arrow column arrow-right"></div>
+        </div>
+      </div>
     );
   }
 }
