390d6e92ded3e5013d4cbed47fd7078468d3ffd0
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-08-02 23:47:59 +0000

    revision.

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -9,12 +9,12 @@ function uniqueLinear(words) {
     i++                             // 2 * n = O(n)
   ) {
     const word = words[i];          // 2 * n = O(n)
-    if (!seen[word]) {              // 1 * 1 = O(1)
-      seen[word] = true;
+    if (!seen[word]) {              // 2 * n = O(n)
+      seen[word] = true;            // 1 * n = O(n)
       unique[unique.length] = word; // 2 * n = O(n)
     }
   }
-  return unique;
+  return unique;                    // 1 * 1 = 0(1)
 } // Big O Notation for uniqueLinear: O(n)
 
 function uniqueQuadratic(words) {
@@ -25,20 +25,20 @@ function uniqueQuadratic(words) {
     i++                             // 2 * n = O(n)
   ) {
     const word = words[i];          // 2 * n = O(n)
-    let isUnique = true;            // 1 * 1 = O(1)
+    let isUnique = true;            // 1 * n = O(n)
     for (
-      let c = 0;                    // 1 * 1 = O(1)
-      c < i;                        // 2 * n = O(n)
-      c++                           // 2 * n = O(n)
+      let c = 0;                    // 1 * n = O(n)
+      c < i;                        // 1 * n^2 = O(n^2)
+      c++                           // 2 * n^2 = O(n^2)
     ) {
-      const comparing = words[c];   // 2 * n = O(n)
-      if (comparing === word) {     // 1 * 1 = O(1)
-        isUnique = false;           // 1 * 1 = O(1)
+      const comparing = words[c];   // 2 * n^2 = O(n^2)
+      if (comparing === word) {     // 1 * n^2 = O(n^2)
+        isUnique = false;           // 1 * n^2 = O(n^2)
       }
     }
-    if (isUnique) {                 // 1 * 1 = O(1)
+    if (isUnique) {                 // 1 * n = O(n)
       unique[unique.length] = word; // 2 * n = O(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
-} // Big O Notation for uniqueQuadratic: O(n)
+} // Big O Notation for uniqueQuadratic: O(n^2)
